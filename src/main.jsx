088df55aaec085af17a2ba73af1fdec9968dfe44
kuku <kuku@MacBook-Air-M3---Tyny.local>
2026-02-12 20:53:13 +0600

    pwa manifest settings

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,9 @@ import App from './App.jsx'
 import './i18n.js'
 import store from './store'
 import { Provider } from 'react-redux'
+import { registerSW } from 'virtual:pwa-register'
+
+registerSW({ immediate: true })
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
