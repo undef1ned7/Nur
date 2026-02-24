75f546ed356f360db5ea77c9c06006e5f73d1f02
kuku <kuku@MacBook-Air-M3---Tyny.local>
2026-02-24 11:57:57 +0600

    post setting page

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,4 +1,13 @@
-export default function logger(log, message) {
+export default function logger(log, ...message) {
     if (process.env.NODE_ENV !== 'development') return;
-    log(message)
+    log(...message)
+}
+
+
+export const info = (...message) => {
+    logger(console.log, ...message);
+}
+
+export const error = (...message) => {
+    logger(console.error, ...message);
 }
