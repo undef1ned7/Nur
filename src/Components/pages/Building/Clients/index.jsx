ddfa5d5946c7c510cdab316061efc0a7b2eefd51
undef1ned7 <myizambekov.baiel9@gmail.com>
2026-02-27 23:24:10 +0600

    add adaptive

--- src/Components/pages/Building/Clients/index.jsx
+++ src/Components/pages/Building/Clients/index.jsx
@@ -193,7 +193,7 @@ export default function BuildingClients() {
   }, [list, search, onlyActive]);
 
   return (
-    <div className="building-page">
+    <div className="building-page building-page--clients">
       <div className="building-page__header">
         <div>
           <h1 className="building-page__title">Клиенты строительства</h1>
